1b98d4afc07b4d4df4e697335dbbc8ef66c2de8f
Muhammad Aasim <muhammadaasimsoomro@gmail.com>
2021-12-24 20:47:15 +0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,10 @@ import './App.css'
 function App () {
   const queryClient = new QueryClient()
 
-  
+
   return (
     <QueryClientProvider client={queryClient}>
+      
       <div style={{ display: 'flex' }}>
         <div className='item'>Item 1</div>
         <div className='item'>Item 2</div>
